8611bdab6488609aa3efca2cd85730d308824702
Archibold Bernard <e.archibold@webforgegh.com>
2020-06-04 07:31:22 +0000

    scanner clear

--- src/com/thearchibold/Basics/StandardInOut2.java
+++ src/com/thearchibold/Basics/StandardInOut2.java
@@ -1,9 +1,21 @@
 package com.thearchibold.Basics;
 
+import java.util.Scanner;
+
 public class StandardInOut2 {
 
+    public static final Scanner scanner = new Scanner(System.in);
     public static void main(String args[]){
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        String s = scanner.nextLine();
 
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 
 }
